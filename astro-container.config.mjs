7ce7b178c773827ac7c774a4b668bc0ceaf7c518
Cihad <cihadp@gmail.com>
2023-05-05 03:19:37 +0300

    a/b test

---
+++ astro-container.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from './custom-config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.rejenerasyon.com.tr',
+  experimental: {
+    assets: true
+  }
+})
--- start-script.js
+++ start-script.js
@@ -9,17 +9,43 @@ const astroFile = path.resolve("node_modules/astro/astro.js");
 
 const scriptContents = fs.readFileSync(astroFile).toString();
 
-const searchQuery = "'use strict';\n";
-const sliceIndex = scriptContents.indexOf(searchQuery) + searchQuery.length;
+const importStr =  `import "${__dirname}/override-fs.js";`;
 
-const importStr =  `import "${__dirname}/override-fs.js";`,
+if (process.argv[2] == "remove") {
+  if (scriptContents.includes(importStr)) {
+    const sliceIndex = scriptContents.indexOf(importStr);
+    const overridedAstro = [
+      scriptContents.slice(0, sliceIndex),
+      scriptContents.slice(sliceIndex + importStr.length)
+    ].join("\n");
+    fs.writeFileSync(astroFile, overridedAstro);
+  }
 
-if (!scriptContents.includes(importStr)) {
-  const overridedAstro = [
-    scriptContents.slice(0, sliceIndex),
-    scriptContents.slice(sliceIndex)
-  ].join("\n");
+  fs.readdirSync(path.resolve("src/assets")).forEach(file => {
+    if (file.startsWith("_") && [".jpg", ".png", ".jpeg"].includes(path.extname(file))) {
+      fs.renameSync(path.resolve("src/assets", file), path.resolve("src/assets", file.slice(1)))
+    }
+  })
+
+} else {
   
-  fs.writeFileSync(astroFile, overridedAstro);
-}
+  const searchQuery = "'use strict';\n";
+  const sliceIndex = scriptContents.indexOf(searchQuery) + searchQuery.length;
+  
+  
+  if (!scriptContents.includes(importStr)) {
+    const overridedAstro = [
+      scriptContents.slice(0, sliceIndex),
+      importStr,
+      scriptContents.slice(sliceIndex)
+    ].join("\n");
+    
+    fs.writeFileSync(astroFile, overridedAstro);
+  }
 
+  fs.readdirSync(path.resolve("src/assets")).forEach(file => {
+    if (!file.startsWith("_") && [".jpg", ".png", ".jpeg"].includes(path.extname(file))) {
+      fs.renameSync(path.resolve("src/assets", file), path.resolve("src/assets", "_" + file))
+    }
+  })
+}
